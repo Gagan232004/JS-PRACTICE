085399405734b26c3b268760658c234e61b97d74
Gagan Santhosh Shetty <153422944+Gagan232004@users.noreply.github.com>
2024-08-25 12:26:01 +0000

    conversion and datatype done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3 + 3) //we are using node js not browser
+
+console.log(3 + 3); //to increase the code readability
+
+console.log("Gagan");
+
+let name = "Gagan" //string datatype
+let num = 7 //number datatype // its range is 2 to power 53
+let isLoggedIn = true //boolean datatype
+
+// if number is too big we use bigint 
+//different datatypes
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//above are primitive datatypes
+
+//object
+
+console.log(typeof "Gagan") //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1 ; false => 0
+//null => 0
+//undefined => NaN
+
+
+let LoggedIn = ""
+let booleanIsLoggedIn = Boolean(LoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false 
+// "gagan" => true
+
+
+let num = 33
+let stringNumber = String(num)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
